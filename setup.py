6a9c4d1f82b369193a87f93747176354ec1edc68
trentm <trentm@da24e2f2-c7f7-11dd-89f9-7f0a91924d23>
2009-01-06 01:29:40 +0000

    an improvement on setup.py; still requires work on embedding content.types in preprocess.py

--- setup.py
+++ setup.py
@@ -1,70 +1,68 @@
 #!/usr/bin/env python
-# Copyright (c) 2002-2005 ActiveState Corp.
-# See LICENSE.txt for license details.
-# Author:
-#   Trent Mick (TrentM@ActiveState.com)
-# Home:
-#   http://trentm.com/projects/preprocess/
+# Copyright (c) 2002-2005 ActiveState Software Ltd.
 
-"""Distutils setup script for 'preprocess'."""
+"""preprocess: a multi-language preprocessor
 
-import sys
+There are millions of templating systems out there (most of them
+developed for the web). This isn't one of those, though it does share
+some basics: a markup syntax for templates that are processed to give
+resultant text output.  The main difference with `preprocess.py` is
+that its syntax is hidden in comments (whatever the syntax for comments
+maybe in the target filetype) so that the file can still have valid
+syntax. A comparison with the C preprocessor is more apt.
+
+`preprocess.py` is targetted at build systems that deal with many
+types of files. Languages for which it works include: C++, Python,
+Perl, Tcl, XML, JavaScript, CSS, IDL, TeX, Fortran, PHP, Java, Shell
+scripts (Bash, CSH, etc.) and C#. Preprocess is usable both as a
+command line app and as a Python module.
+"""
+
 import os
-import shutil
+import sys
+import distutils
 from distutils.core import setup
 
-
-#---- support routines
-
-def _getVersion():
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
+try:
     import preprocess
-    return preprocess.__version__
+finally:
+    del sys.path[0]
 
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Operating System :: OS Independent
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Text Processing :: Filters
+"""
 
-def _getBinDir():
-    """Return the current Python's bindir."""
-    if sys.platform.startswith("win"):
-        bindir = sys.prefix
-    else:
-        bindir = os.path.join(sys.prefix, "bin")
-    return bindir
+if sys.version_info < (2, 3):
+    # Distutils before Python 2.3 doesn't accept classifiers.
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+        _setup(**kwargs)
 
-def _getSiteLibDir():
-    if sys.platform.startswith("win"):
-        sitelibdir = os.path.join(sys.prefix, "Lib", "site-packages")
-    else:
-        ver = '.'.join(map(str, sys.version_info[:2]))
-        sitelibdir = os.path.join(sys.prefix, "lib", "python%s" % ver,
-                                  "site-packages")
-    return sitelibdir
+doclines = __doc__.split("\n")
+script = (sys.platform == "win32" and "bin\\preprocess.exe" or "bin/preprocess")
 
-
-#---- setup mainline
-
-if sys.platform.startswith('win'):
-    scripts = []
-    binFiles = ["preprocess.exe", "preprocess.py"]
-else:
-    if os.path.exists("preprocess"):
-        os.remove("preprocess")
-    shutil.copy2("preprocess.py", "preprocess")
-    scripts = ["preprocess"]
-    binFiles = []
-siteFiles = ["content.types"]
-
-setup(name="preprocess",
-      version=_getVersion(),
-      description="a multi-language preprocessor",
-      author="Trent Mick",
-      author_email="TrentM@ActiveState.com",
-      url="http://trentm.com/projects/preprocess/",
-      license="MIT License",
-      platforms=["Windows", "Linux", "Mac OS X", "Unix"],
-      keywords=["preprocess", "preprocessor", "#if", "#else", "#endif"],
-
-      py_modules=['preprocess'],
-      scripts=scripts,
-      data_files=[ (_getBinDir(), binFiles + siteFiles),
-                   (_getSiteLibDir(), siteFiles) ],
-     )
+setup(
+    name="preprocess",
+    version=preprocess.__version__,
+    maintainer="Trent Mick",
+    maintainer_email="trentm@gmail.com",
+    url="http://code.google.com/p/preprocess/",
+    license="http://www.opensource.org/licenses/mit-license.php",
+    platforms=["any"],
+    py_modules=["preprocess"],
+    package_dir={"": "lib"},
+    scripts=[script],
+    description=doclines[0],
+    classifiers=filter(None, classifiers.split("\n")),
+    long_description="\n".join(doclines[2:]),
+)
 
